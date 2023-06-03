7c8ea44210375f4121e571b536061144a2614edf
Ljf227 <ljfrotton@gmail.com>
2023-06-02 21:01:24 -0400

    ask the user for the size

--- java/data structures/arrays/workingwitharr.java
+++ java/data structures/arrays/workingwitharr.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 class workingwitharr{
 
     public static void Main(String args[]){
+        Scanner stdin = new Scanner(System.in); 
 
+        int size; 
+
+        System.out.print("How big do you want the array to be: "); 
+        size = stdin.nextInt(); 
         
+
     }
 }
