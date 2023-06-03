9d8b0f5fa79b1c3ab5a0a79cb4317ffd1bc6750d
Ljf227 <ljfrotton@gmail.com>
2023-06-02 21:06:36 -0400

    basically done with file

--- java/data structures/arrays/workingwitharr.java
+++ java/data structures/arrays/workingwitharr.java
@@ -9,7 +9,16 @@ class workingwitharr{
 
         System.out.print("How big do you want the array to be: "); 
         size = stdin.nextInt(); 
-        
+
+        // creating the array
+
+        String[] cars = new String[size]; 
+
+        for(int i = 0; i < size; i++){
+            System.out.print("Enter an element of the array: "); 
+            cars[i] = stdin.nextLine(); 
+        }
+
 
     }
 }
