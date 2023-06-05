98130d6000991985dbfc4c120777b59a440a2d95
Ljf227 <ljfrotton@gmail.com>
2023-06-05 09:07:46 -0400

    building a project to practice working with arrrays

--- java/data structures/arrays/resizearray.java
+++ java/data structures/arrays/resizearray.java
@@ -3,7 +3,22 @@ import java.util.Scanner;
 public class resizearray {
     public static void main(String args[]){
         Scanner stdin = new Scanner(System.in); 
+        int size; 
+
+        System.out.println("How big do you want the array to be: "); 
+        size = stdin.nextInt(); 
+        stdin.nextLine(); 
         
+
+
+
+    }
+
+    static String getArr(String arr[]){
+
+    }
+
+    static int getSize(String arr[]){
 
     }
 }
