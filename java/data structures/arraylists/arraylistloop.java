d1a04ba51e2a5a7703be0459b289e4dc7cefa58e
Ljf227 <ljfrotton@gmail.com>
2023-06-09 12:13:38 -0400

    added the exit case

--- java/data structures/arraylists/arraylistloop.java
+++ java/data structures/arraylists/arraylistloop.java
@@ -4,15 +4,19 @@ public class arraylistloop {
     public static void main(String[] args){
         boolean keepGoing = true; 
         String temp; 
+        String temp2; 
 
         ArrayList<String> item = new ArrayList<>(); 
         Scanner stdin = new Scanner(System.in); 
         
         while(keepGoing == true){
-            System.out.println("Enter the element you wish to add here: ");
+            System.out.print("Enter the element you wish to add here: ");
             temp = stdin.nextLine(); 
             item.add(temp); 
-
+            System.out.print("Do you want to keep going: "); 
+            
+            temp2 = stdin.nextLine(); 
+            keepGoing = keepItUp(temp2); 
         }
         stdin.close(); 
 
