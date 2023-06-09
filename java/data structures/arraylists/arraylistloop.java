7243d50725b89f6e2d50493f49bb7b7956d86b3a
Ljf227 <ljfrotton@gmail.com>
2023-06-09 12:11:11 -0400

    continue the file

--- java/data structures/arraylists/arraylistloop.java
+++ java/data structures/arraylists/arraylistloop.java
@@ -1,6 +1,30 @@
 import java.util.ArrayList;
+import java.util.Scanner; 
 public class arraylistloop {
     public static void main(String[] args){
+        boolean keepGoing = true; 
+        String temp; 
+
+        ArrayList<String> item = new ArrayList<>(); 
+        Scanner stdin = new Scanner(System.in); 
         
+        while(keepGoing == true){
+            System.out.println("Enter the element you wish to add here: ");
+            temp = stdin.nextLine(); 
+            item.add(temp); 
+
+        }
+        stdin.close(); 
+
+    }
+
+    static boolean keepItUp(String str){
+        if(str.equals("yes") || str.equals("y") || str.equals("true")){
+            return true; 
+        }
+        else{
+            return false;
+        }
+
     }
 }
