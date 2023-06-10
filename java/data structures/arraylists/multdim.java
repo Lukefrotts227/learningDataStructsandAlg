d11f8f5bae1aea3dd5fe225b3b6aaf1e7406344f
Ljf227 <ljfrotton@gmail.com>
2023-06-10 17:11:31 -0400

    starting the building loop

--- java/data structures/arraylists/multdim.java
+++ java/data structures/arraylists/multdim.java
@@ -1,7 +1,19 @@
 import java.util.ArrayList;
+import java.util.Scanner; 
 public class multdim {
     public static void main(String[] args){
         ArrayList<ArrayList<Integer>> myList = new ArrayList<>(); 
+        Scanner stdin = new Scanner(System.in); 
+        boolean keepGoing = true;
+
+        // build the arraylist 
+        while(keepGoing){
+            
+        }
         
+
+
+
     }
+
 }
