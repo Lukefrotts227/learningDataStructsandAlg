5514087a15907a9826e188d2b2fb8f3539c44e7b
Ljf227 <ljfrotton@gmail.com>
2023-06-10 17:01:37 -0400

    creating an arraylist of arraylist of integers

--- java/data structures/arraylists/multdim.java
+++ java/data structures/arraylists/multdim.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
 public class multdim {
-    
+    public static void main(String[] args){
+        ArrayList<ArrayList<Integer>> myList = new ArrayList<>(); 
+        
+    }
 }
